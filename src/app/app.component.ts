14a6921a932b6d6fed883222bb5e99ecfa490622
Alex <alemat13@gmail.com>
2021-11-08 12:47:25 +0100

    34

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
 import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemons'
 
 @Component({
   selector: 'pokemon-app',
   template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent  {
+export class AppComponent implements OnInit  {
   name = 'Angular';
 
   private pokemons: Pokemon[];
+
+  ngOnInit() {
+    this.pokemons = POKEMONS;
+  }
+
 }
