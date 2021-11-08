1d6abf62faa703621f72daa2ec382cdcc52be6e8
Alex <alemat13@gmail.com>
2021-11-08 12:33:27 +0100

    32

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'pokemon-app',
   template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  name = 'Angular';
+
+  private pokemons: Pokemon[];
+}
---
+++ src/app/pokemon.ts
@@ -0,0 +1,9 @@
+export class Pokemon {
+  id: number;
+  hp: number;
+  cp: number;
+  name: string;
+  picture: string;
+  type: Array<string>;
+  created: Date;
+}
