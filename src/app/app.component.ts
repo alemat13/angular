188beaab81458e93480e1bba0737f70dae64cf05
Alex <alemat13@gmail.com>
2021-11-08 14:08:02 +0100

    46

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent implements OnInit  {
 
   private pokemons: Pokemon[] = null;
   private title: string = "Liste des Pokémons";
+  private value = '';
 
   ngOnInit() {
     this.pokemons = POKEMONS;
@@ -20,6 +21,10 @@ export class AppComponent implements OnInit  {
     console.log('clic!');
   }
 
+  onKey(event: KeyboardEvent) {
+    this.value = 'Bonjour ' + (<HTMLInputElement>event.target).value;
+    console.log(event);
+  }
   selectPokemon(pokemon: Pokemon) {
     alert("Pokemon sélectionné : " + pokemon.name);
   }
