28b057838c6f788342ec5251ca0a5f11107175ab
Alex <alemat13@gmail.com>
2021-11-12 16:57:50 +0100

    88 correctifs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,5 @@
 import { NgModule }               from '@angular/core';
 import { RouterModule, Routes }   from '@angular/router';
-<<<<<<< HEAD
-=======
-// import { ListPokemonComponent }   from './list-pokemon.component';
-// import { DetailPokemonComponent } from './detail-pokemon.component';
->>>>>>> 87dfb420bff1264644c0b549a902730777ac60c7
 import { PageNotFoundComponent }  from './page-not-found.component'
 
 // routes
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-=======
-import { Pokemon } from './pokemons/pokemon';
-import { POKEMONS } from './pokemons/mock-pokemons';
->>>>>>> 87dfb420bff1264644c0b549a902730777ac60c7
 
 @Component({
   selector: 'pokemon-app',
